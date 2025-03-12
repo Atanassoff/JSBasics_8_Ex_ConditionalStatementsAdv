90d48dcf05752c4ea6c5492eb76ae699d480cbaf
Atanassoff <50939943+Atanassoff@users.noreply.github.com>
2025-03-12 15:56:58 +0200

    Delete JSF_Ex_9_7_LowerOrUpper.js

--- JSF_Ex_9_7_LowerOrUpper.js
+++
@@ -1,9 +0,0 @@
-function lowerOrUpper(ch){
-    let x = ch.charCodeAt(0);
-    console.log(x >= 65 && x <= 90 ? "upper-case" : "lower-case");
-}
-
-lowerOrUpper('L') 
-//upper-case
-lowerOrUpper('f') 
-//lower-case 
